1c73521282af89e0987a248ea51c6b80b9113f9e
Rejell <rejell@gmail.com>
2022-10-26 15:21:52 -0300

    Switch Case

---
+++ src/condicionalEncadeada/PlanoOperadora.java
@@ -0,0 +1,21 @@
+package condicionalEncadeada;
+
+public class PlanoOperadora {
+
+    public static void main(String[] args) {
+        String plano = "M";
+
+        if (plano == "B") {
+            System.out.println("100 minutos de ligação");
+        }
+        else if (plano == "M") {
+            System.out.println("100 minutos de ligação");
+            System.out.println("WhatsApp e Instagram grátis");
+        }
+        else if (plano == "T") {
+            System.out.println("100 minutos de ligação");
+            System.out.println("WhatsApp e Instagram grátis");
+            System.out.println("5Gb Youtube");
+        }
+    }
+}
