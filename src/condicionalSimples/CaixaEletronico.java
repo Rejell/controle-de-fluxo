7469b0678b7b874c2af32e3fe295ff52d7d29928
Rejell <rejell@gmail.com>
2022-10-26 14:16:55 -0300

    First commit

---
+++ src/condicionalSimples/CaixaEletronico.java
@@ -0,0 +1,17 @@
+package condicionalSimples;
+
+public class CaixaEletronico {
+
+    public static void main(String[] args) {
+
+        double saldo = 25.0;
+        double valorSolicitado = 26.0;
+
+        if(valorSolicitado < saldo)
+            saldo = saldo - valorSolicitado;
+
+        System.out.println(saldo);
+
+    }
+
+}
