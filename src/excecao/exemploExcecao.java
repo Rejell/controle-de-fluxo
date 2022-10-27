f7e25f2323215b9fc70e1c8611fb1e3b9d9f3308
Rejell <rejell@gmail.com>
2022-10-27 10:07:13 -0300

    Tratamento de exceções

---
+++ src/excecao/exemploExcecao.java
@@ -0,0 +1,40 @@
+package excecao;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exemploExcecao {
+    public static void main(String[] args) {
+
+        try {
+
+            // Criando o objeto scanner
+            Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+            System.out.println("Digite seu nome");
+            String nome = scanner.next();
+
+            System.out.println("Digite seu sobrenome");
+            String sobrenome = scanner.next();
+
+            System.out.println("Digite sua idade");
+            int idade = scanner.nextInt();
+
+            System.out.println("Digite sua altura");
+            double altura = scanner.nextDouble();
+
+            // Imprimindo os dados obtidos pelo usuário
+            System.out.println("Olá, me chamo " + nome.toUpperCase() + " " + sobrenome.toUpperCase() + ".");
+            System.out.println("Tenho " + idade + " anos.");
+            System.out.println("Minha altura é " + altura + " m.");
+
+        }
+
+        catch (InputMismatchException e){
+            System.err.println("O campo idade e altura precisam ser numéricos e, quando necessário, usar ponto (.) como separador de casas decimais.");
+        }
+
+    }
+}
+
